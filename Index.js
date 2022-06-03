7760a2195ad4eeaba9509cf2df6622becdcf9edd
Arellano995 <noearellano443@gmail.com>
2022-06-03 11:31:29 -0500

    proyecto base

---
+++ Index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+ });
+
+ $(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+ });
+
+ $(document).ready(function(){
+    $('.modal').modal();
+  });
+
+ $(document).ready(function(){
+   $('.collapsible').collapsible();
+ });
+
+ $(document).ready(function(){
+   $('.dropdown-trigger').dropdown();
+ });
